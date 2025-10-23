e4d5836875a405757bf109f4e573688f6f141711
chen guo <guoc3125@gmail.com>
2025-10-23 13:52:27 +0800

    Change ViewAttendance to ViewAttendees and fix bugs

--- src/main/java/seedu/address/logic/commands/ViewAttendeesCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewAttendeesCommand.java
@@ -20,13 +20,13 @@ public class ViewAttendeesCommand extends Command {
     public static final String COMMAND_WORD = "viewattendees";
     private final EventId eventId;
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Views attendees for an event. "
-            + "Parameters: e/EVENTID\n"
-            + "Example: " + COMMAND_WORD + " e/Orientation2023";
-
     public static final String MESSAGE_EVENT_NOT_FOUND = "Event not found";
     public static final String MESSAGE_SUCCESS = "Attendees for %1$s:\n%2$s";
     public static final String MESSAGE_NO_ATTENDANCE = "No attendance recorded yet.";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Views attendees for an event. "
+            + "Parameters: e/EVENTID\n"
+            + "Example: " + COMMAND_WORD + " e/Orientation2023";
 
     /**
      * Creates a ViewAttendeesCommand to view attendees for the specified event.
