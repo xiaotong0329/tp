311db5ede4db2bed96df8382d8a476bf69307ccd
chen guo <guoc3125@gmail.com>
2025-10-28 16:09:34 +0800

    Update testcases

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -53,7 +53,7 @@ public class FindCommandTest {
 
     @Test
     public void execute_zeroKeywords_noPersonFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0) + "\n\n(Use 'list' to go back)";
         PersonContainsKeywordsPredicate predicate = preparePredicate(" ");
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredPersonList(predicate);
@@ -67,7 +67,7 @@ public class FindCommandTest {
         FindCommand command = new FindCommand(predicate);
         expectedModel.updateFilteredPersonList(predicate);
         String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW,
-                expectedModel.getFilteredPersonList().size());
+                expectedModel.getFilteredPersonList().size()) + "\n\n(Use 'list' to go back)";
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertTrue(model.getFilteredPersonList().size() >= 1);
     }
